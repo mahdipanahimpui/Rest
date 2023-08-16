bdfa6e2674cee0c442dbfff1b013ba474c2b90c0
mahdi <mahdipanahimpui@gmail.com>
2023-08-16 20:49:20 +0330

    model_serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 # is a third way to validate
@@ -6,12 +7,31 @@ def check_email(value):
     if 'admin' in value:
         raise serializers.ValidationError('admin word must not be at email')
 
+
 # serializer is used to convert to json and reverse to django model
-class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True, validators=[check_email])
-    password = serializers.CharField(required=True)
-    confirm_password = serializers.CharField(required=True)
+# class UserRegisterSerializer(serializers.Serializer):
+#     username = serializers.CharField(required=True)
+#     email = serializers.EmailField(required=True, validators=[check_email])
+#     password = serializers.CharField(required=True)
+#     confirm_password = serializers.CharField(required=True)
+
+
+#### use model serializer ####
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = User
+        # exclude = ('email', ) # all exclude the username, dont use fields again
+        # fields = '__all__' # to get all fields
+        fields = ('username', 'email', 'password', 'confirm_password')
+
+        # to make readonly the password
+        extra_kwargs = {
+            'password' : {'write_only':True}, # wont be shown in the return Response(ser_data.data) >> for confirm_password ABOVE
+            'email': {'validators': (check_email,)}
+        }
 
     # valus is the field that is sent
     ## field level validation
@@ -21,7 +41,6 @@ class UserRegisterSerializer(serializers.Serializer):
         return value
 
         
-    
     # data is required
     ## object level validation
     def validate(self, data):
