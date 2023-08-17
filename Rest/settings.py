4bc7db2d8cbf8215cba4d37c9efde9b09c6478ca
mahdi <mahdipanahimpui@gmail.com>
2023-08-17 20:11:20 +0330

    renderer_parser

--- Rest/settings.py
+++ Rest/settings.py
@@ -175,6 +175,11 @@ REST_FRAMEWORK = {
 
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
+    # renderer: to render the response that is returned to client
+    # parser: to send data from client to server
+    # 'DEFAULT_RENDERER_CLASSES': [
+    #     'rest_framework.renderers.JSONRenderer'
+    # ],
 
 
 }
